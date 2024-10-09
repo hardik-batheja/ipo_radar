d864bcef6591ad0784b4736c04d52c54443c4647
hardik-batheja <47427640+hardik-batheja@users.noreply.github.com>
2024-10-09 16:57:40 +0530

    Def_db

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py makemigrations
-python manage.py migrate
+python manage.py migrate --run-syncdb
 if [[ $CREATE_SUPERUSER ]];
 then
   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
